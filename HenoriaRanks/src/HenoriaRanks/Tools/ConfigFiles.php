8f5b7f09c410d85e3448de33e406731927b6264a
Verre2OuiSki <48868686+Verre2OuiSki@users.noreply.github.com>
2021-01-31 02:57:12 +0100

    update config files

--- HenoriaRanks/src/HenoriaRanks/Tools/ConfigFiles.php
+++ HenoriaRanks/src/HenoriaRanks/Tools/ConfigFiles.php
@@ -8,17 +8,12 @@ use pocketmine\utils\Config;
 class ConfigFiles{
 
 
-    const CONFIG = "pluginConfig";
     const RANKS_CONFIG = [ "ranksConfig.json", "resource", Config::JSON ];
     const PLAYERS_RANK = [ "playersRank.yml", Config::YAML ];
 
 
 
     public static function getTable( $table ){
-
-        if( $table === self::CONFIG ){
-            return Main::getInstance()->getConfig();
-        }
 
         if( $table[1] === "resource" ){
             Main::getInstance()->saveResource( $table[0] );
